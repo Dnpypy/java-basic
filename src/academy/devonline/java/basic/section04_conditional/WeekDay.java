89dc78078a88e633d2897b93450725863fc849c0
Dnpypy <denlatypoff@yandex.ru>
2021-11-27 19:38:24 +0300

    SimpleCalculator, WeekDay

---
+++ src/academy/devonline/java/basic/section04_conditional/WeekDay.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.basic.section04_conditional;
+
+import java.util.Scanner;
+
+/**
+ * "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+ * @author Dnpypy
+ * @link http://devonline.academy/java-basic
+ */
+public class WeekDay {
+    public static void main(String[] args) {
+        // read source data
+        Scanner sc = new Scanner(System.in);
+        //var day = sc.next();
+        var day = sc.next().charAt(0);
+
+        // processing
+        String result;
+        if (day == '1') {
+            result = "Monday";
+        } else if (day == '2') {
+            result = "Tuesday";
+        } else if (day == '3') {
+            result = "Wednesday";
+        } else if (day == '4') {
+            result = "Thursday";
+        } else if (day == '5') {
+            result = "Friday" ;
+        } else if (day == '6')  {
+            result = "Saturday";
+        } else if (day == '7') {
+            result = "Sunday" ;
+        } else {
+            result = "not day: " + day;
+        }
+
+        // display results
+        System.out.println(result);
+    }
+}
