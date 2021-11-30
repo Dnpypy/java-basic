68017ffcdfad1d3eae48cb17c6e6ff9610786c6f
Dnpypy <denlatypoff@yandex.ru>
2021-11-30 04:12:26 +0300

    DisplayMaxNumber, Switch, WeekDayWithSwitch, ConditionalOperators

---
+++ src/academy/devonline/java/basic/section04_conditional/WeekDayWithSwitch.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2019. http://devonline.academy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package academy.devonline.java.basic.section04_conditional;
+
+
+import java.util.Scanner;
+
+/**
+ * "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+ * @author Dnpypy
+ * @link http://devonline.academy/java-basic
+ */
+public class WeekDayWithSwitch {
+    public static void main(String[] args) {
+        var day = new Scanner(System.in).nextInt();
+
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Unsupported day number: " + day);
+        }
+    }
+}
