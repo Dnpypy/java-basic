2b0090797a4e3c2fbc4983f66f4331ba18a1ccf7
Dnpypy <denlatypoff@yandex.ru>
2021-12-23 18:46:00 +0300

    Add section function, BigIntegerHome

--- src/academy/devonline/java/basic/section07_String/BigIntegerSumHome.java
+++ src/academy/devonline/java/basic/section07_String/BigIntegerSumHome.java
@@ -38,13 +38,37 @@ import java.util.Scanner;
 public class BigIntegerSumHome {
     public static void main(String[] args) {
         // read source data
-        var bigA = new Scanner(System.in).nextLine();
-        var bigB = new Scanner(System.in).nextLine();
+        var a = "123456789012345678901234567890123456789012345678901234567890";
+        var b = "12345678901234567890123456789012345678901234567890";
 
         //processing
+        var num1 = a.length() >= b.length() ? a : "0".repeat(b.length() - a.length()) + a;
+        var num2 = b.length() >= a.length() ? b: "0".repeat(a.length() - b.length()) + b;
 
+        StringBuilder res = new StringBuilder();
+        int prevDigit = 0;
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            var digit1 = Integer.parseInt(String.valueOf(num1.charAt(i)));
+            var digit2 = Integer.parseInt(String.valueOf(num2.charAt(i)));
+            int sum = digit1 + digit2 + prevDigit;
+            if (sum > 9) {
+                prevDigit = 1;
+                sum = sum % 10;
+            } else {
+                prevDigit = 0;
+            }
+            res.insert(0, sum);
+        }
+        if (prevDigit != 0) {
+            res.insert(0, prevDigit);
+        }
+
+        // display results
+        System.out.println(res.toString());
+        System.out.println(new BigInteger(a).add(new BigInteger(b)));
 
         //display results
-
+        System.out.println(res.toString());
+        System.out.println(new BigInteger(a).add(new BigInteger(b)));
     }
 }
